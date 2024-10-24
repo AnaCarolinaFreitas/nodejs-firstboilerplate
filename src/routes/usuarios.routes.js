820e64d4236335f4a4f0e605d2e74788e6e34984
Ana Carolina <anacarolinagarciafreitas@gmail.com>
2024-10-24 11:38:48 -0300

    feat: create get all users

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -6,4 +6,13 @@ let usuarios = [
 
 ];
 
+// Rota para listar todos os usuarios
+usuariosRoutes.get("/", (req, res) => {
+    return res.status(200).json({
+        message: usuarios.length == 0 
+        ? "Não há usuarios cadastrados"
+        : `Total de usuários: ${usuarios.length}`
+    });
+  });
+
 export default usuariosRoutes;
